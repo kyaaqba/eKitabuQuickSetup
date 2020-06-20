6ab7cad862ab6c1321d78815539a10e470a0b42f
Karam Yaaqba <v-kayaaq@microsoft.com>
2020-06-20 16:18:00 -0700

    first commit

---
+++ quicksetup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "      _  ___ _        _            __          __  _"                               
+echo "     | |/ (_) |      | |           \ \        / / | |"                                
+echo "  ___| ' / _| |_ __ _| |__  _   _   \ \  /\  / /__| |__  ___  ___ _ ____   _____ _ __" 
+echo " / _ \  < | | __/ _  |  _ \| | | |   \ \/  \/ / _ \  _ \/ __|/ _ \  __\ \ / / _ \  __|"
+echo "|  __/ . \| | || (_| | |_) | |_| |    \  /\  /  __/ |_) \__ \  __/ |   \ V /  __/ |   "
+echo " \___|_|\_\_|\__\__,_|_.__/ \__,_|     \/  \/ \___|_.__/|___/\___|_|    \_/ \___|_| "
+echo ""
+echo $"Update apt-get\n"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt install zip unzip
+echo $"Install vagrant\n"
+sudo apt-get install -y vagrant
+echo $'Install virtualbox\n'
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
+sudo apt -y update
+sudo apt install -y virtualbox-6.0
+echo $'Downloading vagrant box\n'
+fileid="1GF989-PJBgifZOVd0DgPEBvPRBWLIuWA"
+filename="virtualbox.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+echo $'Unzip vagrant box\n'
+sudo unzip virtualbox.zip
+echo $'Navigate to vagrant box directory'
+cd virtualbox/
+echo $"$(tput setaf 1) Use Vagrant to create a Virtual Machine (--name is to give the output a name, and the name could be modified)\n"
+read -r -s -p $'Press enter to continue...\n'
+sudo vagrant box add --name "webserver" --force ubuntu-18.04-0.1.box
+echo $"$(tput setaf 1) Use Vagrant to initialize the Virtual Machine\n"
+read -r -s -p $'Press enter to continue...\n'
+sudo vagrant init --force webserver
+echo $"$(tput setaf 1) Start up the virtual machine\n"
+read -r -s -p $'Press enter to continue...\n'
+sudo vagrant up
+echo $"$(tput setaf 1) Access the virtual machine through ssh\n"
+read -r -s -p $'Press enter to continue...\n'
+sudo vagrant ssh 
+
+
